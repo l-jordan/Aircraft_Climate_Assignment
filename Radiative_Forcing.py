bdf7782c06f4d990b5f1d2aff77ffecd12272b10
IvoLor <127728278+IvoLor@users.noreply.github.com>
2025-02-19 10:04:47 +0100

    Update Radiative_Forcing.py

--- Radiative_Forcing.py
+++ Radiative_Forcing.py
@@ -3,3 +3,15 @@ import numpy as np
 
 # Longwave radiative flux change due to a contrail
 # Beer-Lambert Law
+
+tau = 0.1
+T = 288.15 # K
+sigma = 5.670374419 * 10**(-8) # W/m^2/K^4 
+
+F_in = sigma * T**4
+
+F_out = F_in *np.exp(-tau)
+
+
+RFC_LW = F_out_nocontrail - F_out_contrail
+RFC_LW2 = F_in * (1 - np.exp(-tau))
